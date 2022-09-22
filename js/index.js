b5bd7891c524245899b9a4a1b7dd3042b785269a
Miquel Cardiel <mcardielv@hotmail.com>
2022-09-22 23:00:36 +0200

    form fixes

--- js/index.js
+++ js/index.js
@@ -19,39 +19,129 @@ function calculateAll() {
 
   // ITERATION 2
   const allProducts = document.querySelectorAll('.product');
-  console.log(allProducts);
+  let subTotal = 0;
 
-  for (i = 0; i < allProducts.length; i++) {
-    updateSubtotal(allProducts[i]);
-  }
+  allProducts.forEach((product) => {
+    subTotal += updateSubtotal(product);
+  });
 
   // // ITERATION 3
-  const superTotal = document.getElementById('total-value').innerText;
+  const superTotal = document.getElementById('total-value');
   console.log(superTotal);
-
-  for (i = 0; i < allProducts.length; i++) {
-    superTotal.innerHTML += superResult;
-  }
-  return superTotal.innerHTML;
+  superTotal.innerText = '$' + subTotal;
 }
-
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
-  //... your code goes here
+
+  const removeChild = target.parentNode.parentNode;
+  console.log(removeChild);
+
+  const parent = removeChild.parentNode;
+  console.log(parent);
+
+  parent.removeChild(removeChild);
+  calculateAll();
 }
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+  const name = document.querySelectorAll('.create-product input')[0].value;
+  console.log(name);
+  const price = document.querySelectorAll('.create-product input')[1].value;
+
+  createNewProductRow(name, price);
 }
+
+function createNewProductRow(name, price) {
+  const cart = document.querySelector('tbody');
+
+  const trProduct = document.createElement('tr');
+  trProduct.className = 'product';
+
+  const tdName = document.createElement('td');
+  const nameSpan = document.createElement('span');
+  tdName.className = 'name';
+  nameSpan.innerText = name;
+  tdName.appendChild(nameSpan);
+
+  const tdPrice = document.createElement('td');
+  tdPrice.className = 'price';
+  tdPrice.innerText = '€';
+  const tdPriceContent = document.createElement('span');
+  tdPriceContent.innerText = price;
+  tdPrice.appendChild(tdPriceContent);
+
+  const tdQuantity = document.createElement('td');
+  tdQuantity.className = 'quantity';
+  const quantityInput = document.createElement('input');
+  quantityInput.type = 'number';
+  quantityInput.value = 0;
+  quantityInput.placeholder = 'Qauntity';
+  quantityInput.min = 0;
+  tdQuantity.appendChild(quantityInput);
+
+  const tdSubtotal = document.createElement('td');
+  tdSubtotal.className = 'subtotal';
+  tdSubtotal.innerText = '€';
+  const subtotalSpan = document.createElement('span');
+  subtotalSpan.innerText = 0;
+  tdSubtotal.appendChild(subtotalSpan);
+
+  const tdAction = document.createElement('td');
+  tdAction.className = 'action';
+  const buttonAction = document.createElement('button');
+  buttonAction.className = 'btn btn-remove';
+  buttonAction.innerText = 'Remove';
+  buttonAction.addEventListener('click', removeProduct);
+  tdAction.appendChild(buttonAction);
+
+  trProduct.appendChild(tdName);
+  trProduct.appendChild(tdPrice);
+  trProduct.appendChild(tdQuantity);
+  trProduct.appendChild(tdSubtotal);
+  trProduct.appendChild(tdAction);
+
+  cart.appendChild(trProduct);
+}
+
+//other way to copy html structure directly
+// function createProduct() {
+//   const productName = document.querySelector('.product-name');
+//   const productPrice = document.querySelector('.product-price');
+
+//   const addToList = document.querySelector('tbody');
+//   addToList.innerHTML += `
+//       <tr class="product">
+//       <td class="name">
+//         <span>${productName.value}</span>
+//       </td>
+//       <td class="price">$<span>${productPrice.value}</span></td>
+//       <td class="quantity">
+//         <input type="number" value="0" min="0" placeholder="Quantity" />
+//       </td>
+//       <td class="subtotal">$<span>0</span></td>
+//       <td class="action">
+//         <button class="btn btn-remove">Remove</button>
+//       </td>
+//     </tr>
+//   `;
+
+//   //clearing the input fields
+//   productName.value = '';
+//   productPrice.value = 0;
+// }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+  document.querySelectorAll('btn-remove').forEach((productButton) => {
+    productButton.addEventListener('click', removeProduct);
+  });
+
+  document.querySelector('#create').addEventListener('click', createProduct);
 });
